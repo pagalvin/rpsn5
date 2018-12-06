28b923ab937ae72a0535d9bf0ce80177e49f34c7
Paul Galvin <Paul.Galvin@neudesic.com>
2018-12-06 09:39:07 -0500

    Computer starts to make more rational peace time choices.

--- src/Game/ComputerPlayer.ts
+++ src/Game/ComputerPlayer.ts
@@ -6,6 +6,20 @@ import { Rng } from "../Utils/Rng";
 import { MilitaryBaseFactory } from "../Factories/MilitaryBaseFactory";
 import { Game } from "../Entities/gameEntity";
 import { MapUtil } from "../Utils/MapUtils";
+import { Constants } from "./constants";
+
+interface peaceTimeSentimentMapping {
+    percentBuildStart: number,
+    percentDeclareWarStart: number,
+    percentSpyStart: number;
+}
+
+/*
+    Underinformed: doesn't know enough about enemy map
+    Falling Behind: doesn't think it has enough bases
+    Confident: feels good about the mix of things.
+*/
+type sentiments = "Underinformed" | "Falling Behind" | "Confident";
 
 export class ComputerPlayer extends AbstractPlayer implements GamestateWatcher {
 
@@ -15,7 +29,9 @@ export class ComputerPlayer extends AbstractPlayer implements GamestateWatcher {
     public declaredWar: boolean;
     public Name: string;
     public map: CountryMap;
-    public totalPopulationDied: number = 0;
+    public totalCausualties: number = 0;
+
+    private sentimentMap: Map<sentiments,peaceTimeSentimentMapping>;
 
     constructor() {
 
@@ -28,10 +44,15 @@ export class ComputerPlayer extends AbstractPlayer implements GamestateWatcher {
         this.declaredWar = false;
         this.totalFunctionalActiveRadarStations = 0;
         this.totalFunctionalPassiveRadarStations = 0;
-        this.totalPopulationDied = 0;
+        this.totalCausualties = 0;
 
         MapUtil.createTestBases({ onMap: this.map });
 
+        this.sentimentMap = new Map<sentiments, peaceTimeSentimentMapping>();
+        this.sentimentMap.set("Underinformed", {percentBuildStart: 25, percentSpyStart: 26, percentDeclareWarStart: 99});
+        this.sentimentMap.set("Falling Behind", {percentBuildStart: 75, percentSpyStart: 76, percentDeclareWarStart: 99});
+        this.sentimentMap.set("Confident", {percentBuildStart: 49, percentSpyStart: 50, percentDeclareWarStart: 98});
+        
     }
 
     public handleGamestateChange(args: { details: gameStateChangeDetails }) {
@@ -201,26 +222,83 @@ export class ComputerPlayer extends AbstractPlayer implements GamestateWatcher {
 
     }
 
+    private calculateCurrentSentiment(): sentiments {
+
+        const game = Game.getInstance();
+
+        const opm = game.humanPlayer.map;
+
+        // Figure out how many known/suspected bases there are.
+        const suspectedBasesCount = opm.getAllMilitaryBases().filter(b => b.myMapLocation.enemyVisibility > Constants.PCT_VISIBLE_SENTIMENT_THRESHOLD).length;
+
+        const expectedBaseCount = Math.ceil( (game.gameYear - GameRules.GameStartYear) * Constants.EXPECTED_BASE_COUNT_FACTOR);
+
+        let result: sentiments;
+
+        // If we expect [x] bases but there are more than that many suspected then we're falling behind.
+        if (expectedBaseCount > (suspectedBasesCount * 2)) result = "Falling Behind";
+        else if (expectedBaseCount > (suspectedBasesCount * 3)) result = "Underinformed";
+        else result = "Confident";
+
+        console.log(`ComputerPlayer.ts: calculateCurrentSentiment: suspected bases, expected bases:`, {sentiment: result, expectedBaseCount: expectedBaseCount, suspected: suspectedBasesCount});
+
+        return result;
+     }
+
     private playPeacefulTurn() {
 
-        const availableStrategicChoices = GameRules.getAllowedMoves().strategicOptions;
+        /*
+            Principles:
+
+            At a high level, computer should:
+                - Declare war if it's feeling very confident or at a significant disadvantage
+                - It should spy if it feels like it doesn't have enough information about the other player
+                - It should build if it feels "OK"
+
+            So, it knows that at most, there are going to be 2x bases times game years.
+            It can assess where it stands on that number over time and aim to be at like 1.5 or 1.75 total bases per game year
+
+            It can look at what it "knows" about the opponent and if doesn't know enough, it gets nervous.
+                - Make a "target knowns" per year
+                - Later years, it expects to know about [x] amount of bases that the user has
+
+            Idea:
+            - determine current sentiment
+            - get a lookup table based on sentiment
+            - pick a random number and looku up a choice based on that.
+
+        */
+        // const availableStrategicChoices = GameRules.getAllowedMoves().strategicOptions;
+
+        const sentiment = this.calculateCurrentSentiment();
+
+        const sentimentTable: Exclude<peaceTimeSentimentMapping,undefined> = <Exclude<peaceTimeSentimentMapping,undefined>>this.sentimentMap.get(sentiment);
+
+        const tablelookupVal = Rng.throwDice({hiNumberMinus1: 99});
+
+        const strategicChoice: strategicMoveOptions = 
+           tablelookupVal >= sentimentTable.percentDeclareWarStart 
+            ? "Declare War" 
+            : tablelookupVal >= sentimentTable.percentSpyStart
+                ? "Spy"
+                : "Build";
 
         // Need to pick a strategic choice
-        const choice = <strategicMoveOptions>Rng.pickRandomFromArray({ sourceArray: availableStrategicChoices });
+        // const choice = <strategicMoveOptions>Rng.pickRandomFromArray({ sourceArray: availableStrategicChoices });
 
-        console.log(`ComputerPlayer: playTurn: my strategic choice is:`, choice);
+        console.log(`ComputerPlayer: playTurn: my strategic choice is:`, strategicChoice);
 
-        if (choice === "Build") {
+        if (strategicChoice === "Build") {
             GameLogic.spyOnPlayer({targetPlayer: Game.getInstance().humanPlayer, spyLevel: 1})
             this.buildBases({ numberToBuild: 2, allowedBases: GameRules.getAllowedMoves().tacticalOptions });
         }
 
-        else if (choice === "Spy") {
+        else if (strategicChoice === "Spy") {
             GameLogic.spyOnPlayer({targetPlayer: Game.getInstance().humanPlayer, spyLevel: 2})
             this.buildBases({ numberToBuild: 1, allowedBases: GameRules.getAllowedMoves().tacticalOptions });
         }
 
-        else if (choice === "Declare War") {
+        else if (strategicChoice === "Declare War") {
             console.log(`ComputerPlayer.ts: playTurn: Declaring war!`);
         }
         else {
