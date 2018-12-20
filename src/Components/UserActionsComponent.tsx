4b37ce39e409f8cd2efef06b682b1c9a8babb3ac
Paul Galvin <Paul.Galvin@neudesic.com>
2018-12-20 08:28:16 -0500

    Cut out some experimental code.

--- src/Components/UserActionsComponent.tsx
+++ src/Components/UserActionsComponent.tsx
@@ -36,21 +36,6 @@ interface actionHandlerMappings {
 
 export class UserActionsComponent extends Component<props, state> implements GamestateWatcher, playerMapClickListener {
 
-    // readonly StyledButton = withStyles({
-    //     root: {
-    //       background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    //       borderRadius: 3,
-    //       border: 0,
-    //       color: 'blue',
-    //       height: 48,
-    //       padding: '0 30px',
-    //       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    //     },
-    //     label: {
-    //       textTransform: 'capitalize',
-    //     },
-    //   })(Button);
-      
     private uiIdx: number = 0;
     private readonly uiKey = () => `useractions${this.uiIdx++}`;
 
