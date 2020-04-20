aed7ce775cef702278b32cbd4af150968de98c5b
Paul Galvin <pjgalvin@pplweb.com>
2020-04-20 11:49:05 -0400

    Added a list of air base names to the factory.

--- src/Factories/BaseNameFactories/AirBaseNamesFactory.ts
+++ src/Factories/BaseNameFactories/AirBaseNamesFactory.ts
@@ -6,6 +6,34 @@ export class AirBaseNamesFactory extends AbstractBaseNameFactory {
     computerPrefix = "B";
     
     availableBaseNames: string[] = [
+        "Lady Hawke",
+        "Hugin",
+        "Munin",
+        "Iago",
+        "Matthew",
+        "Raven",
+        "Hedwig",
+        "Phoenix",
+        "Owl",
+        "Raptor",
+        "Elwing",
+        "Fawlkes",
+        "Jareth",
+        "Scuttle",
+        "Archimedes",
+        "Gwythaint",
+        "Thorondor",
+        "Reginal Hoopoe",
+        "Quoth",
+        "Filthy Lucre",
+        "Skarlath",
+        "Mr. Big",
+        "Senu",
+        "Nevermore",
+        "Crone",
+        "Mortimer",
+        "Motley",
+        "Kehaar"
     ];
 
     public static getInstance() {
--- src/Game/BuildVersion.ts
+++ src/Game/BuildVersion.ts
@@ -1,1 +1,1 @@
-export class BuildVersion { public static readonly BUILD_VERSION = "72 at  9:47:10.79 on Mon 04/20/2020"; } 
+export class BuildVersion { public static readonly BUILD_VERSION = "73 at 10:27:08.52 on Mon 04/20/2020"; } 
