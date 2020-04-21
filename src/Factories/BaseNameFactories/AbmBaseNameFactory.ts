a3984593f582ee8196b3af4fe75af941cc5cf39f
Paul Galvin <pjgalvin@pplweb.com>
2020-04-21 08:46:43 -0400

    ABM's get their own pool of unique names.

--- src/Factories/BaseNameFactories/AbmBaseNameFactory.ts
+++ src/Factories/BaseNameFactories/AbmBaseNameFactory.ts
@@ -5,6 +5,28 @@ export class AbmBaseNameFactory extends AbstractBaseNameFactory  {
     computerPrefix = "A";
     
     availableBaseNames: string[] = [
+        "Sicilian",
+        "King's Pawn",
+        "Alekhine",
+        "Ruy Lopez",
+        "Indian Game",
+        "Hungarian Opening",
+        "King's Indian",
+        "French",
+        "Chigorin",
+        "Petroff",
+        "Queen's Gambit Declined",
+        "Philidor",
+        "Pirc",
+        "Modern",
+        "Queen's Gambit Accepted",
+        "Slav",
+        "Stonewall",
+        "Benoni",
+        "Tarrasch",
+        "Dutch",
+        "Englund",
+        "Grünfeld"
     ];
 
     public static getInstance() {
